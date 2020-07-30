16f205393c915de1d99ff0f3af8c0ffbd9b95f46
David <david.vicente.martinez@alumnos.upm.es>
2020-07-30 13:02:13 +0200

    Equipment class  added

--- economic_assessment_poo.py
+++ economic_assessment_poo.py
@@ -1,7 +1,8 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 
 from numpy_financial import pmt, ipmt, ppmt, npv, irr
+from enum import Enum
 
 data_factors = np.array([[0.3, 0.5, 0.6],
                          [0.8, 0.6, 0.2],
@@ -15,192 +16,108 @@ data_factors = np.array([[0.3, 0.5, 0.6],
                          [0.1, 0.1, 0.1]])
 
 Capital_factors = pd.DataFrame(data_factors,
-                               index=["fer", "fp", "fi", "fel", "fc", "fs", "fl", "OS", "D&E", "X"], 
+                               index=["fer", "fp", "fi", "fel", "fc", "fs", "fl", "OS", "D&E", "X"],
                                columns=["Fluids", "Fluids-Solids", "Solids"])
 
-def boiler(Q, p, fm=1, installed=True):
-    """Return boiler cost. Inputs:
-    Vapor production (kg/h): 5000 < Q < 800000
-    Pressure (bar): 			   10 < p < 70
-    fm = material factor"""
 
-    assert type(installed) == bool
+class Category(Enum):
+    fluids = "Fluids"
+    fluids_solids = "Fluids-Solids"
+    solids = "Solids"
 
-    if Q < 5000 or Q > 800000:
-        print(f"    - WARNING: boiler vapor production out of method bounds, 5000 < Q < 800000. Results may not be accurate.")
 
-    if p < 10 or p > 70:
-        print(f"    - WARNING: boiler pressure out of method bounds, 10 < p < 70. Results may not be accurate.")
+class Equipment:
+    def __init__(self, category, fm=1, installed=True):
+        assert type(installed) == bool
+        assert isinstance(category, Category)
+        self.category = category.value
+        self.fm = fm
+        self.installed = True
 
-    if Q < 20000:
-        C = 106000 + 8.7*Q
-    elif Q < 200000:
-        if p < 15:
-            C = 110000 + 4.5*Q**0.9
-        elif p < 40:
-            C = 106000 + 8.7*Q
+    def applyWilliams(self, cost_ref, cap, cap_ref, n):
+        assert n <= 1
+        assert n >= 0.6
+
+        return cost_ref * pow(cap / cap_ref, n)
+
+    def lang(self, C, Capital_factors):
+        t = self.category
+        C *= ((1 + Capital_factors.loc["fp"][t]) * self.fm + (
+                    Capital_factors.loc["fer"][t] + Capital_factors.loc["fel"][t]
+                    + Capital_factors.loc["fi"][t] + Capital_factors.loc["fc"][t]
+                    + Capital_factors.loc["fs"][t] + Capital_factors.loc["fl"][t]))
+        return C
+
+
+class Boiler(Equipment):
+    def __init__(self, Q, p, category=Category.fluids, fm=1, installed=True):
+        super().__init__(category, fm, installed)
+        self.Q = Q
+        self.p = p
+
+    def boiler(self):
+        if self.Q < 5000 or self.Q > 800000:
+            print(
+                f"    - WARNING: boiler vapor production out of method bounds, 5000 < Q < 800000. Results may not be accurate.")
+        if self.p < 10 or self.p > 70:
+            print(f"    - WARNING: boiler pressure out of method bounds, 10 < p < 70. Results may not be accurate.")
+        if self.Q < 20000:
+            C = 106000 + 8.7 * self.Q
+        elif self.Q < 200000:
+            if self.p < 15:
+                C = 110000 + 4.5 * self.Q ** 0.9
+            elif self.p < 40:
+                C = 106000 + 8.7 * self.Q
+            else:
+                C = 110000 + 4.5 * self.Q ** 0.9
         else:
-            C = 110000 + 4.5*Q**0.9
-    else:
-        C = 110000 + 4.5*Q**0.9
+            C = 110000 + 4.5 * Q ** 0.9
 
-    if installed:
-        C *= ((1+Capital_factors.loc["fp"]["Fluids"])*fm+(Capital_factors.loc["fer"]["Fluids"] + Capital_factors.loc["fel"]["Fluids"]
-                                                        + Capital_factors.loc["fi"]["Fluids"] + Capital_factors.loc["fc"]["Fluids"]
-                                                        + Capital_factors.loc["fs"]["Fluids"] + Capital_factors.loc["fl"]["Fluids"]))
+        if self.installed:
+            C = self.lang(C, Capital_factors)
 
-    return C
+        return C
 
-def pump(Q, fm=1, installed=True):
-    """Return centrifuge pump cost for a caudal between 0.2 and 126 L/s. Inputs:
-    phase = 'Fluids', 'Fluids - Solids' or 'Solids'
-    fm = material factor"""
+class Pump(Equipment):
+    def __init__(self, Q, category=Category.fluids, fm=1, installed=True):
+        super().__init__(category, fm, installed)
+        self.Q = Q
 
-    assert type(installed) == bool
+    def pump(self):
+        if self.Q < 0.2 or self.Q > 126:
+            print(f"    - WARNING: pump caudal out of method bounds, 0.2 < Q (L/s) < 126. Results may not be accurate.")
 
-    if Q < 0.2 or Q > 126:
-        print(f"    - WARNING: pump caudal out of method bounds, 0.2 < Q (L/s) < 126. Results may not be accurate.")
-    
-    C = 6900 + 206*Q**0.9
+        C = 6900 + 206 * self.Q ** 0.9
 
-    if installed:
-        C *= ((1+Capital_factors.loc["fp"]["Fluids"])*fm+(Capital_factors.loc["fer"]["Fluids"] + Capital_factors.loc["fel"]["Fluids"]
-                                                     + Capital_factors.loc["fi"]["Fluids"] + Capital_factors.loc["fc"]["Fluids"]
-                                                     + Capital_factors.loc["fs"]["Fluids"] + Capital_factors.loc["fl"]["Fluids"]))
+        if self.installed:
+            C = self.lang(C, Capital_factors)
 
-    return C
+        return C
 
-def steam_turbine(kW, fm=1, installed=True):
-    """Return steam turbine cost for a power between 100 and 20000 kW. Inputs:
-    fm = material factor"""
+class SteamTurbine(Equipment):
+    def __init__(self, kw, category=Category.fluids, fm=1, installed=True):
+        super().__init__(category, fm, installed)
+        self.kw = kw
 
-    assert type(installed) == bool
-    
-    if kW < 100 or kW > 20000:
-        print(f"    - WARNING: steam turbine power out of method bounds, 100 < kW < 20000. Results may not be accurate.")
-    
-    C = -12000 + 1630*kW**0.75
+    def steam_turbine(self):
+        if self.kw < 100 or self.kw > 20000:
+            print(
+                f"    - WARNING: steam turbine power out of method bounds, 100 < kW < 20000. Results may not be accurate.")
 
-    if installed:
-        C *= ((1+Capital_factors.loc["fp"]["Fluids"])*fm +(Capital_factors.loc["fer"]["Fluids"] + Capital_factors.loc["fel"]["Fluids"]
-                                                         + Capital_factors.loc["fi"]["Fluids"] + Capital_factors.loc["fc"]["Fluids"]
-                                                         + Capital_factors.loc["fs"]["Fluids"] + Capital_factors.loc["fl"]["Fluids"]))
+        C = -12000 + 1630 * self.kw ** 0.75
 
-    return C
+        if self.installed:
+            C = self.lang(C, Capital_factors)
 
-def loan(quantity, interest, years):
-    """Compute annual payment of a loan. Inputs:
-    quantity [monetary units] == investment which will be funded
-    interest [as fraction of unity] == annual interest
-    years == number of yeras to return the loan."""
+        return C
 
-    assert quantity > 0
-    assert interest >= 0 and interest <= 1
-    assert years > 1
-
-    loan_payment   = pmt(interest, years, quantity)
-    loan_interest  = ipmt(interest, np.arange(years) + 1, years, quantity)
-    loan_principal = ppmt(interest, np.arange(years) + 1, years, quantity)
-
-    return loan_payment, loan_interest, loan_principal
-
-def depreciation(annual_percent, capex, residual_value=0):
-    """Compute annual depreciation of investment. Inputs:
-    annual_percent [as fraction of unity] == annual percent of depreciation.
-    capex [monetary units] == capital expenditure
-    residual_value[monetary units] == plant value at the end of its life."""
-
-    assert annual_percent >= 0 and annual_percent <= 1
-
-    annual_depreciation = []
-    prev = 1
-
-    while True:
-        if prev < annual_percent:
-            annual_depreciation.append(prev)
-            break
-        annual_depreciation.append(annual_percent)
-        prev = prev - annual_percent
-
-    depreciation_array = -1 * np.array(annual_depreciation) * (capex - residual_value)
-
-    return depreciation_array
 
 if __name__ == '__main__':
+    print(Capital_factors)
 
-    # Calculating capital costs (CAPEX)
-    boiler    = boiler(10000, 70)
-    turbine   = steam_turbine(1500)
-    condenser = 400000 * (10000 / 15000)**0.8
-    pump      = pump(2.84)
+    boiler  = Boiler(10000, 70).boiler()
+    turbine = SteamTurbine(1500).steam_turbine()
+    condenser = Equipment(Category.fluids).applyWilliams(400000,10000,15000,0.8)
+    pump = Pump(2.84).pump()
 
-    capex = boiler + turbine + condenser + pump
-
-    # Calculating operational costs (OPEX)
-    capacity_factor = 0.9
-
-    water       = 1.29 * 10 * 8760 * capacity_factor
-    salaries    = 4 * 3 * 30000
-
-    # Calculating loan
-    quantity = 0.6 * capex
-    _, interest, principal = loan(quantity, 0.04, 10)
-
-    # Calculating depreciation
-    dep_array = depreciation(0.07, capex) 
-
-    # Calculating sales
-    sales = 1500 * 0.05 * 8760 * capacity_factor
-
-    # Calculating financial model
-    years = 20
-
-    investment    = np.array([-capex*0.4] + [0 for i in range(years-1)])
-    depreciation  = np.hstack(([0], dep_array, [0 for i in range(years-1-len(dep_array))]))
-    loan_prin     = np.hstack(([0], principal, [0 for i in range(years-1-len(principal))]))
-    loan_int      = np.hstack(([0], interest, [0 for i in range(years-1-len(interest))]))
-
-    sales_array    = np.zeros(years)
-    water_array    = np.zeros(years)
-    salaries_array = np.zeros(years)   
-
-    for i in range(years):
-        if i == 0:
-            sales_array[i]    = 0
-            water_array[i]    = 0
-            salaries_array[i] = 0
-        elif i == 1:
-            sales_array[i]    = sales
-            water_array[i]    = -1*water
-            salaries_array[i] = -1*salaries
-        else:
-            sales_array[i]    = sales_array[i-1]*1.03
-            water_array[i]    = water_array[i-1]*1.03
-            salaries_array[i] = salaries_array[i-1]*1.02
-
-    ebt   = np.vstack((investment, depreciation, loan_int, sales_array, water_array, salaries_array)).sum(axis=0)
-    taxes = ebt * -0.3
-    for i in range(len(taxes)):
-        if taxes[i] > 0:
-            taxes[i] = 0
-    eat = ebt - taxes
-    cash_flow = eat - depreciation + loan_prin
-    cumulative_cash_flow = np.cumsum(cash_flow)
-
-    data = np.vstack((investment, sales_array, depreciation, loan_prin, loan_int, salaries_array, water_array, ebt, 
-                      taxes, eat, cash_flow, cumulative_cash_flow))
-    df   = pd.DataFrame(data,
-                        index=['Investment', 'Sales', 'Depreciation', 'Loan principal', 'Loan interest', 'Salaries',
-                               'Water', 'EBT', 'Taxes', 'EAT', 'Cash Flow', 'Cumulative Cash Flow'],
-                        columns=[i for i in range(years)])
-
-    
-    # Calculating financial metrics
-    discount_rate = 0.053
-
-    npv = npv(discount_rate, cash_flow)
-    irr = irr(cash_flow)
-
-    # Printing results
-    print(df)
-    print(f"The project has a net present value of {'{:,.2f}'.format(npv)}€ and an internal rate of return of {round(irr*100, 2)}%")
+    print(boiler + turbine + condenser + pump)
